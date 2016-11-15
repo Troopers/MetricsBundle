720c7d75adfc0234f40bfa4c62a68f932160b04d
Leny BERNARD <leny@troopers.email>
2016-11-15 13:57:02 +0100

    :white_check_mark: add DataTransformer and DI unit tests

---
+++ Tests/DataTransformer/JsonToArrayTransformerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Troopers\MetricsBundle\Tests\DependencyInjection;
+
+use Troopers\MetricsBundle\DataTransformer\JsonToArrayTransformer;
+
+class TransformerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test the transformer on a low level
+     */
+    public function testForwardTransform()
+    {
+        $transformer = new JsonToArrayTransformer();
+
+        $array = [
+            'item', 'item2', 'item3'
+        ];
+
+        $json = json_encode($array);
+
+        $this->assertEquals(
+            $json,
+            $transformer->transform($array)
+        );
+    }
+}
