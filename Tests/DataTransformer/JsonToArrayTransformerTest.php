4715be033d3f82e6bfa40ad61583ef829e56b5f5
Leny BERNARD <lenybernard@users.noreply.github.com>
2016-11-15 14:05:36 +0100

    :rotating_light: Applied fixes from StyleCI (#7)

--- Tests/DataTransformer/JsonToArrayTransformerTest.php
+++ Tests/DataTransformer/JsonToArrayTransformerTest.php
@@ -4,17 +4,17 @@ namespace Troopers\MetricsBundle\Tests\DependencyInjection;
 
 use Troopers\MetricsBundle\DataTransformer\JsonToArrayTransformer;
 
-class TransformerTest extends \PHPUnit_Framework_TestCase
+class JsonToArrayTransformerTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Test the transformer on a low level
+     * Test the transformer on a low level.
      */
     public function testForwardTransform()
     {
         $transformer = new JsonToArrayTransformer();
 
         $array = [
-            'item', 'item2', 'item3'
+            'item', 'item2', 'item3',
         ];
 
         $json = json_encode($array);
